f33127c97ce9824307ac11cc3103b3259eb7433e
owen <orowader@infoviewsystems.com>
2022-03-16 21:47:05 -0400

    big fixes

--- res_script.js
+++ res_script.js
@@ -1,18 +1,25 @@
 function loadres() {
 	
+	//load results from answers on index.html 
+	
+	//get the current results 
 	jsarray = JSON.parse(sessionStorage.getItem("jsarray")); 
+	
+	//load results history
 	var allEnters = JSON.parse(sessionStorage.getItem("jsarray_all")); 
 	var entries =JSON.parse(sessionStorage.getItem("entries")); 
 	
+	//if there is no history yet, initialize it 
 	if (allEnters == null) {
 		
 		entries  = 1; 
-		
 		allEnters = []; 
 		allEnters[entries - 1] = jsarray; 
 		sessionStorage.setItem("jsarray_all", JSON.stringify(allEnters));
 		sessionStorage.setItem("entries", entries);
 	}
+	
+	//add to history if it exists 
 	else {
 		entries += 1
 		allEnters[entries - 1] = jsarray; 
@@ -20,6 +27,7 @@ function loadres() {
 		sessionStorage.setItem("entries", entries);
 	}
 	
+	//print results to screen 
 	result.textContent = jsarray[24]; 
 	char1.textContent = jsarray[18]; 
 	char2.textContent = jsarray[19]; 
--- return_script.js
+++ return_script.js
@@ -1,3 +1,5 @@
 function return_page() {
+	
+	//just send back to home 
 	window.location.href = 'index.html';
 }
