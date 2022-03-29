8d2ea03ed6fadc80eb6b11cd31443417007405ff
owen <orowader@infoviewsystems.com>
2022-03-29 14:56:30 -0400

    fixes

--- script.js
+++ script.js
@@ -101,58 +101,6 @@ function store_res(char, jsarray, class_name) {
 }
 
 
-function calcOneThruThree(q, counters) {
-	
-	naCount = counters[0];
-	points = counters[1];
-	
-	
-	 for (var i = 1; i <= 3; ++i) { //for q2-4
-      if (q[i][0].checked)
-        if (i == 1) {       //q2 yes is selected
-          points += 5;
-        }
-        else if (i == 2) {  //q3 yes is selected
-          points += 1;
-        }
-        else {              //q4 yes is selected
-          points += 1;     
-        }
-      else if (q[i][1].checked)
-        if (i == 1) {      //q2 no is selected (CRITICAL)
-          points -= 100;
-        }
-        else if (i == 2) { //q3 no is selected
-          points -= 3;
-        }
-        else {             //q4 no is selected
-          points -= 3; 
-        }
-      else if (q[i][2].checked)
-        if (i == 1) {
-          points -= 100;   //q2 N/A is selected (CRITICAL)
-          naCount++;
-        }
-        else if (i == 2) { //q3 N/A is selected
-          points += 0;
-          naCount++;
-        }
-        else {             //q4 N/A is selected
-          points += 0;
-          naCount++;
-        }
-       //A Question was left unanswered raises a flag, so the program will prompt
-       //the user to fill out any empty q's before it calculates an answer 
-      else { 
-        flag = true;
-      }
-    }
-	
-	counters[0] = naCount;
-	counters[1] = points; 
-}
-
-
 //print results of a form 
 function compute_res(flag, naCount, char, jsarray, class_name){
 	//if a question was left blank 
@@ -197,7 +145,6 @@ function isClass() {
 
   var points = 0;
   var naCount = 0;
-  counters = [naCount, points]; 
   var char = new Array(false, false, false, false, false, false, false);
   var flag = false;
   var jsarray = init_jsarray();
@@ -255,10 +202,48 @@ function isClass() {
     */ 
 	var i = 0
 	
-	calcOneThruThree(q, counters);
-	naCount = counters[0];
-	points = counters[1] 
 	
+    for (i = 1; i <= 3; ++i) { //for q2-4
+      if (q[i][0].checked)
+        if (i == 1) {       //q2 yes is selected
+          points += 5;
+        }
+        else if (i == 2) {  //q3 yes is selected
+          points += 1;
+        }
+        else {              //q4 yes is selected
+          points += 1;     
+        }
+      else if (q[i][1].checked)
+        if (i == 1) {      //q2 no is selected (CRITICAL)
+          points -= 100;
+        }
+        else if (i == 2) { //q3 no is selected
+          points -= 3;
+        }
+        else {             //q4 no is selected
+          points -= 3; 
+        }
+      else if (q[i][2].checked)
+        if (i == 1) {
+          points -= 100;   //q2 N/A is selected (CRITICAL)
+          naCount++;
+        }
+        else if (i == 2) { //q3 N/A is selected
+          points += 0;
+          naCount++;
+        }
+        else {             //q4 N/A is selected
+          points += 0;
+          naCount++;
+        }
+       //A Question was left unanswered raises a flag, so the program will prompt
+       //the user to fill out any empty q's before it calculates an answer 
+      else { 
+        flag = true;
+      }
+    }
+
     //if points is positive it passes the characteristic test 
     if (points >= 0) {
       char[1] = true;
